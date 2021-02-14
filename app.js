52d5a00a26d309d2fdc587b2134f0c298d12400a
RemoteSoul-Hub <yahya.tahoune@gmail.com>
2021-02-14 22:50:01 +0100

    pseudo code and structure for better workflow

---
+++ app.js
@@ -0,0 +1,18 @@
+let name = document.getElementById('name');
+let date = document.getElementById('date');
+let amount = document.getElementById('amount');
+let add = document.getElementById('submit');
+let table = document.getElementById('table');
+
+
+/// Event listener to Name
+
+//Event Listener to Date
+
+// Event listener to Amount
+
+
+//Submit Function
+
+
+// Add new Tr to table
