8affb6dc8960c924af140292e77776954e82a9da
RemoteSoul-Hub <yahya.tahoune@gmail.com>
2021-02-17 01:46:09 +0100

    Functionnality to add table rows with the entry elements

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ let table = document.getElementById('table');
 
 
 /// Event listener to Name
+function addItem(e) {
+    e.preventDefault();
+    // Get value of input
+    let newName = document.getElementById('name').value;
+    let newDate = document.getElementById('date').value;
+    let newAmount = document.getElementById('amount').value;
+    let row = table.insertRow(1);
+    let cell0 = row.insertCell(0);
+    let cell1 = row.insertCell(1);
+    let cell2 = row.insertCell(2);
+    cell0.innerHTML = newName;
+    cell1.innerHTML = newDate;
+    cell2.innerHTML = newAmount;
+}
 
 //Event Listener to Date
 
+
+
 // Event listener to Amount
 
-
+add.addEventListener('click', addItem);
 //Submit Function
 
 
